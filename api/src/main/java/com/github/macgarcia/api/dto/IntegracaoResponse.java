c61662c79354cb244050c4be132992e3299e44c7
macgarcia <marquim.iron@gmail.com>
2023-07-02 17:34:51 -0300

    Correção, retirada do construtor.

--- api/src/main/java/com/github/macgarcia/api/dto/IntegracaoResponse.java
+++ api/src/main/java/com/github/macgarcia/api/dto/IntegracaoResponse.java
@@ -2,10 +2,4 @@ package com.github.macgarcia.api.dto;
 
 public record IntegracaoResponse(int codigo, String metodo, String erro) {
 
-	public IntegracaoResponse(int codigo, String metodo, String erro) {
-		this.codigo = codigo;
-		this.metodo = metodo;
-		this.erro = erro;
-	}
-
 }
