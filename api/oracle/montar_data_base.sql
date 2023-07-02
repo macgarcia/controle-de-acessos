a6041417a501334a9ad3929d1cbc76b4adb26caa
macgarcia <marquim.iron@gmail.com>
2023-07-02 16:34:20 -0300

    Alterações na base de dados, desabilitada a trigger de gerar histórico

--- api/oracle/montar_data_base.sql
+++ api/oracle/montar_data_base.sql
@@ -1,4 +1,4 @@
 @@C:\Users\macgarcia\Desktop\scripts_oracle\script_tabelas.sql;
 @@C:\Users\macgarcia\Desktop\scripts_oracle\HistoricoNotaType.obj;
 @@C:\Users\macgarcia\Desktop\scripts_oracle\LogNotaType.obj;
-@@C:\Users\macgarcia\Desktop\scripts_oracle\tgr_insere_historico.sql;
+-- @@C:\Users\macgarcia\Desktop\scripts_oracle\tgr_insere_historico.sql;
