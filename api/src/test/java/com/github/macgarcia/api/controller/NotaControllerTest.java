8d8a577118d9698cae457c1a34a118e2598a6ce4
macgarcia <marquim.iron@gmail.com>
2023-07-04 14:01:44 -0300

    Adicionado testes na API e mudança na estrutura do projeto

---
+++ api/src/test/java/com/github/macgarcia/api/controller/NotaControllerTest.java
@@ -0,0 +1,78 @@
+package com.github.macgarcia.api.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.github.macgarcia.api.dto.IntegracaoResponse;
+import com.github.macgarcia.api.dto.NotaDtoEntrada;
+import com.github.macgarcia.api.service.NotaService;
+
+@MockitoSettings
+@ExtendWith(MockitoExtension.class)
+public class NotaControllerTest {
+
+	@InjectMocks
+	NotaController controller;
+
+	@Mock
+	private NotaService service;
+
+	MockMvc mockMvc;
+
+	private NotaDtoEntrada dto;
+
+	@BeforeEach
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).alwaysDo(print()).build();
+		dto = new NotaDtoEntrada(null, null, null, null, null, null, null, null, null);
+	}
+
+	@Test
+	void testeIntegracaoDeDados() throws Exception {
+
+		doNothing().when(service).processar(dto);
+		
+		ResponseEntity<IntegracaoResponse> response = controller.integrarDadosDeNota(dto);
+		
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals("Integração de dados", response.getBody().metodo());
+        assertEquals("Não houve erros", response.getBody().erro());
+        
+        verify(service, times(1)).processar(dto);
+
+	}
+	
+	@Test
+	void testeDeErroIntegracaoDeDados() throws Exception {
+		
+		Exception e = new Exception("Erro de integração");
+		
+		doThrow(e).when(service).processar(dto);
+		
+		ResponseEntity<IntegracaoResponse> response = controller.integrarDadosDeNota(dto);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+        assertEquals("Integração de dados", response.getBody().metodo());
+        assertEquals("Erro de integração", response.getBody().erro());
+
+        verify(service, times(1)).processar(dto);
+		
+	}
+
+}
---
+++ desktop/MYSQL/script_tables.sql
@@ -0,0 +1,55 @@
+drop table if exists nota;
+
+create table nota ( id int auto_increment
+	, descricao varchar(50) null
+	, titulo varchar(50) not null
+	, data_criacao datetime not null
+	, usuario varchar(50) not null
+	, senha varchar(50) not null
+	, url_site varchar(200) null
+	, data_atualizacao datetime
+	, constraint pk_nota primary key (id)
+);
+
+drop table if exists historico_nota;
+
+create table historico_nota ( id int auto_increment
+	, id_nota int
+	, data_validade_inicial datetime not null
+	, data_validade_final datetime not null
+	, numero_atualizacao int not null
+	, descricao varchar(50) null
+	, titulo varchar(50) not null
+	, data_criacao datetime not null
+	, usuario varchar(50) not null
+	, senha varchar(50) not null
+	, url_site varchar(200) null
+	, data_atualizacao datetime
+	, constraint pk_nota primary key (id)
+);
+
+alter table historico_nota
+   add constraint pk_historico_nota foreign key (id_nota)
+   references nota(id);
+
+alter table historico_nota
+    drop column data_atualizacao;
+
+alter  table historico_nota
+   modify id_nota int not null;
+   
+alter table nota
+   add column flag_integrado int not null -- 1 - não integrado, 0 - integrado
+
+drop table if exists configuracao;
+
+create table configuracao ( id int auto_increment
+	, ativar_integracao int -- 1 - Ligado, 0 - desligado
+	, intervalo_integracao int
+	, constraint pk_configuracao primary key(id)
+);
+alter table configuracao
+   add column inicio_imediato int;
+   
+alter table configuracao
+   modify id int;
